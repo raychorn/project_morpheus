9a1a2445d58045f8bd5bec592637ab4c6a80fa2d
Ray C Horn <raychorn@gmail.com>
2021-12-28 13:19:16 -0700

    updates.

---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DIR0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+VENV_DIR=$(ls -la $DIR0/venv*/bin/activate | head -n 1)
+
+MAKEVENV=$DIR0/makevenv.sh
+if [ ! -f "$VENV_DIR" ]; then
+    echo "No virtualenv found. Creating one..."
+    if [ ! -f "$MAKEVENV" ]; then
+        echo "No makevenv.sh found. Exiting..."
+        exit 1
+    fi
+    $MAKEVENV
+    VENV_DIR=$(ls -la $DIR0/venv*/bin/activate | head -n 1)
+fi
+
+if [ ! -f "$MAKEVENV" ]; then
+    echo "No makevenv.sh found. Exiting..."
+    exit 1
+fi
+
+if [ ! -f "$VENV_DIR" ]; then
+    echo "No Virtual Env found. Exiting..."
+    exit 1
+fi
+. $VENV_DIR
+
+PY=$(which python3.9)
+
+if [ ! -f "$PY" ]; then
+    echo "No python3.9 found. Exiting..."
+    exit 1
+fi
+
+$PY $DIR0/morpheus.py
